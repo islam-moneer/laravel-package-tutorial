fdde5dcd445584ee21ccdd3697bcaeb2074c4e81
Islam Moneer <islam.moneer@sourcya.com>
2018-02-15 21:03:12 +0200

    init make view

---
+++ src/MakeViewServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace IslamMoneer\MakeView;
+
+use Illuminate\Support\ServiceProvider;
+class MakeViewServiceProvider  extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/view/display-view.php');
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
+{
+
+}
