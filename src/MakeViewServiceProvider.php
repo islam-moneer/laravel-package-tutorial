2f1f1e355f6b11e6cc923ec1bce97ab392de6afb
Islam Moneer <islam.moneer@sourcya.com>
2018-02-15 21:41:47 +0200

    create simple view

--- src/MakeViewServiceProvider.php
+++ src/MakeViewServiceProvider.php
@@ -11,7 +11,13 @@ class MakeViewServiceProvider  extends ServiceProvider
      */
     public function boot()
     {
+//        Load package logic
         $this->loadRoutesFrom(__DIR__.'/view/display-view.php');
+
+//        Publish views
+        $this->publishes([
+            __DIR__.'/view/views/' => resource_path('myviews'),
+        ]);
     }
     /**
      * Register the application services.
